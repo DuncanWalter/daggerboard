ddc966b62232c73f2264f3985ff232a8319e14ba
DuncanWalter <dwalter@barlingarts.com>
2019-05-13 16:26:47 -0500

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import { Provider } from './Provider'
+import { useHistory, useRouter } from './push'
+
+export { Provider }
+export { useHistory, useRouter }
---
+++ src/types.d.ts
@@ -0,0 +1,38 @@
+import { History } from 'history'
+import { ReactElement } from 'react'
+
+export type Route = ReactElement | string | Router | ThunkRoute | AsyncRoute
+export type Router = { [path: string]: Route }
+type ThunkRoute = (match: ThunkRouteParams) => Route
+interface AsyncRoute extends Promise<Route> {}
+
+interface ThunkRouteParams {
+  params: { [param: string]: string }
+  exact: boolean
+  match: string
+}
+
+export interface MatchRequest {
+  router: Router
+  path: string
+  globalMatch: string
+  localMatch: string
+  params: { [param: string]: string }
+}
+
+export interface MatchResult {
+  route: string | ReactElement | null
+  router: Router
+  path: string
+  globalMatch: string
+  localMatch: string
+  params: { [param: string]: string }
+}
+
+export type AsyncMatchResult = Promise<MatchResult> | MatchResult
+
+export interface DaggerboardContext {
+  history: History
+  currentPath: string
+  isTerminal(route: Route): route is ReactElement
+}
